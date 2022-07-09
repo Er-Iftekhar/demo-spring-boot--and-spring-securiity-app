9a5fabeffc3c4a7ab270c51676ae8ac383483904
SyedIftekharuddin <syedy636@gmail.com>
2022-07-09 17:21:21 +0200

    Initial project commit

---
+++ src/main/java/com/app/demospringsecurityappwithamigo/DemoSpringSecurityAppWithAmigoApplication.java
@@ -0,0 +1,13 @@
+package com.app.demospringsecurityappwithamigo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoSpringSecurityAppWithAmigoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoSpringSecurityAppWithAmigoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/app/demospringsecurityappwithamigo/DemoSpringSecurityAppWithAmigoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.demospringsecurityappwithamigo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoSpringSecurityAppWithAmigoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
